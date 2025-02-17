5fbd3baacdcbd3bfd9fd70e5bd8add9ce3e09537
Marius Bohn <marius.bohn@t-online.de>
2025-02-17 15:55:40 +0100

    Added UNO to train.py

--- mrifield/train/train.py
+++ mrifield/train/train.py
@@ -11,7 +11,7 @@ from magnet_pinn.data.grid import MagnetGridIterator
 from magnet_pinn.data.utils import worker_init_fn
 
 from mrifield.models import UNet3D
-from neuralop.models import FNO
+from neuralop.models import FNO, UNO
 from mrifield.train.lit_mrifield import LitMRIField
 
 os.environ['HTTPS_PROXY'] = 'http://proxy:80'
@@ -20,7 +20,8 @@ TRAIN_DIR = "/anvme/workspace/b190cb19-magnet/processed/train/grid_voxel_size_4_
 VAL_DIR = "/anvme/workspace/b190cb19-magnet/processed/val/grid_voxel_size_4_data_type_float32"
 
 #model = UNet3D(in_channels=5, out_channels=12)
-model = FNO(n_modes=(16, 16, 16), in_channels=5, out_channels=12, hidden_channels=64, positional_embedding=None)
+#model = FNO(n_modes=(16, 16, 16), in_channels=5, out_channels=12, hidden_channels=64, positional_embedding=None)
+model = UNO(in_channels=5, out_channels=12, hidden_channels=16, uno_out_channels=[32,64,64,32], uno_n_modes=[[16,16,16],[16,16,16],[16,16,16],[16,16,16]], uno_scalings=[[1,1,1],[0.5,0.5,0.5],[1,1,1],[2,2,2]], channel_mlp_skip='linear')
 
 train_input_normalizer = StandardNormalizer.load_from_json(f"{TRAIN_DIR}/normalization/input_normalization.json")
 train_target_normalizer = StandardNormalizer.load_from_json(f"{TRAIN_DIR}/normalization/target_normalization.json")
@@ -42,7 +43,7 @@ val_set = MagnetGridIterator(VAL_DIR, transforms=augmentation, num_samples=100)
 train_loader = DataLoader(train_set, batch_size=4, num_workers=16, worker_init_fn=worker_init_fn)
 val_loader = DataLoader(val_set, batch_size=4, num_workers=16, worker_init_fn=worker_init_fn)
 
-wandb_logger = WandbLogger(project='fno', name='Training 37M')
+wandb_logger = WandbLogger(project="uno", name="Training 32M")
 
 trainer = pl.Trainer(accelerator="gpu", devices=1, log_every_n_steps=100, max_epochs=10, logger=wandb_logger)
 trainer.fit(model=lit_model, train_dataloaders=train_loader, val_dataloaders=val_loader)
