cc4d3c30aacf90818678c3984a9531a30ee768f1
Marius Bohn <marius.bohn@t-online.de>
2025-04-17 09:51:29 +0200

    Modified FFNO to be more memory-efficient

--- mrifield/models/ffno/factorized_fno/mesh_3d.py
+++ mrifield/models/ffno/factorized_fno/mesh_3d.py
@@ -11,17 +11,16 @@ from ..feedforward import FeedForward
 from ..linear import WNLinear
 
 
-class SpectralConv2d(nn.Module):
-    def __init__(self, in_dim, out_dim, modes_x, modes_y, modes_z, forecast_ff, backcast_ff,
+class SpectralConv3d(nn.Module):
+    def __init__(self, in_dim, out_dim, modes_x, modes_y, modes_z,
                  fourier_weight, factor, ff_weight_norm,
-                 n_ff_layers, layer_norm, use_fork, dropout):
+                 n_ff_layers, layer_norm, dropout):
         super().__init__()
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.modes_x = modes_x
         self.modes_y = modes_y
         self.modes_z = modes_z
-        self.use_fork = use_fork
 
         self.fourier_weight = fourier_weight
         # Can't use complex type yet. See https://github.com/pytorch/pytorch/issues/59998
@@ -33,81 +32,60 @@ class SpectralConv2d(nn.Module):
                 nn.init.xavier_normal_(param)
                 self.fourier_weight.append(param)
 
-        if use_fork:
-            self.forecast_ff = forecast_ff
-            if not self.forecast_ff:
-                self.forecast_ff = FeedForward(
-                    out_dim, factor, ff_weight_norm, n_ff_layers, layer_norm, dropout)
-
-        self.backcast_ff = backcast_ff
-        if not self.backcast_ff:
-            self.backcast_ff = FeedForward(
+        self.backcast_ff = FeedForward(
                 out_dim, factor, ff_weight_norm, n_ff_layers, layer_norm, dropout)
 
     def forward(self, x):
-        # x.shape == [batch_size, grid_size, grid_size, in_dim]
         x = self.forward_fourier(x)
+        x = self.backcast_ff(x)
 
-        b = self.backcast_ff(x)
-        f = self.forecast_ff(x) if self.use_fork else None
-        return b, f
+        return x
 
     def forward_fourier(self, x):
         x = rearrange(x, 'b s1 s2 s3 i -> b i s1 s2 s3')
-        # x.shape == [batch_size, in_dim, grid_size, grid_size]
 
         B, I, S1, S2, S3 = x.shape
 
-        # # # Dimesion Z # # #
-        x_ftz = torch.fft.rfft(x, dim=-1, norm='ortho')
-        # x_ft.shape == [batch_size, in_dim, grid_size, grid_size // 2 + 1]
+        # # # Dimension Z # # #
+        x_ft = torch.fft.rfft(x, dim=-1, norm='ortho')
 
-        out_ft = x_ftz.new_zeros(B, I, S1, S2, S3 // 2 + 1)
-        # out_ft.shape == [batch_size, in_dim, grid_size, grid_size // 2 + 1, 2]
+        out_ft = x_ft.new_zeros(B, I, S1, S2, S3 // 2 + 1)
 
         out_ft[:, :, :, :, :self.modes_z] = torch.einsum(
             "bixyz,ioz->boxyz",
-            x_ftz[:, :, :, :, :self.modes_z],
+            x_ft[:, :, :, :, :self.modes_z],
             torch.view_as_complex(self.fourier_weight[2]))
 
-        xz = torch.fft.irfft(out_ft, n=S3, dim=-1, norm='ortho')
-        # x.shape == [batch_size, in_dim, grid_size, grid_size]
+        x_out = torch.fft.irfft(out_ft, n=S3, dim=-1, norm='ortho')
 
-        # # # Dimesion Y # # #
-        x_fty = torch.fft.rfft(x, dim=-2, norm='ortho')
-        # x_ft.shape == [batch_size, in_dim, grid_size // 2 + 1, grid_size]
+        # # # Dimension Y # # #
+        x_ft = torch.fft.rfft(x, dim=-2, norm='ortho')
 
-        out_ft = x_fty.new_zeros(B, I, S1, S2 // 2 + 1, S3)
-        # out_ft.shape == [batch_size, in_dim, grid_size // 2 + 1, grid_size, 2]
+        out_ft = x_ft.new_zeros(B, I, S1, S2 // 2 + 1, S3)
 
         out_ft[:, :, :, :self.modes_y, :] = torch.einsum(
             "bixyz,ioy->boxyz",
-            x_fty[:, :, :, :self.modes_y, :],
+            x_ft[:, :, :, :self.modes_y, :],
             torch.view_as_complex(self.fourier_weight[1]))
 
-        xy = torch.fft.irfft(out_ft, n=S2, dim=-2, norm='ortho')
-        # x.shape == [batch_size, in_dim, grid_size, grid_size]
+        x_out += torch.fft.irfft(out_ft, n=S2, dim=-2, norm='ortho')
 
-        # # # Dimesion X # # #
-        x_ftx = torch.fft.rfft(x, dim=-3, norm='ortho')
-        # x_ft.shape == [batch_size, in_dim, grid_size // 2 + 1, grid_size]
+        # # # Dimension X # # #
+        x_ft = torch.fft.rfft(x, dim=-3, norm='ortho')
 
-        out_ft = x_ftx.new_zeros(B, I, S1 // 2 + 1, S2, S3)
-        # out_ft.shape == [batch_size, in_dim, grid_size // 2 + 1, grid_size, 2]
+        out_ft = x_ft.new_zeros(B, I, S1 // 2 + 1, S2, S3)
 
         out_ft[:, :, :self.modes_x, :, :] = torch.einsum(
             "bixyz,iox->boxyz",
-            x_ftx[:, :, :self.modes_x, :, :],
+            x_ft[:, :, :self.modes_x, :, :],
             torch.view_as_complex(self.fourier_weight[0]))
 
-        xx = torch.fft.irfft(out_ft, n=S1, dim=-3, norm='ortho')
-        # x.shape == [batch_size, in_dim, grid_size, grid_size]
+        x_out += torch.fft.irfft(out_ft, n=S1, dim=-3, norm='ortho')
 
         # # Combining Dimensions # #
-        x = xx + xy + xz
+        #x = xx + xy + xz
 
-        x = rearrange(x, 'b i s1 s2 s3 -> b s1 s2 s3 i')
-        # x.shape == [batch_size, grid_size, grid_size, out_dim]
+        x = rearrange(x_out, 'b i s1 s2 s3 -> b s1 s2 s3 i')
 
         return x
 
@@ -124,7 +102,7 @@ class FNOFactorizedMesh3D(nn.Module):
         self.width = width
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.in_proj = WNLinear(input_dim, self.width, wnorm=ff_weight_norm)
+        self.in_lift = WNLinear(input_dim, self.width, wnorm=ff_weight_norm)
         self.n_layers = n_layers
 
         self.fourier_weight = None
@@ -138,55 +116,30 @@ class FNOFactorizedMesh3D(nn.Module):
 
         self.spectral_layers = nn.ModuleList([])
         for _ in range(n_layers):
-            self.spectral_layers.append(SpectralConv2d(in_dim=width,
+            self.spectral_layers.append(SpectralConv3d(in_dim=width,
                                                        out_dim=width,
                                                        modes_x=modes_x,
                                                        modes_y=modes_y,
                                                        modes_z=modes_z,
-                                                       forecast_ff=None,
-                                                       backcast_ff=None,
                                                        fourier_weight=self.fourier_weight,
                                                        factor=factor,
                                                        ff_weight_norm=ff_weight_norm,
                                                        n_ff_layers=n_ff_layers,
                                                        layer_norm=layer_norm,
-                                                       use_fork=False,
                                                        dropout=0.0))
 
-        self.out = nn.Sequential(
+        self.out_proj = nn.Sequential(
             WNLinear(self.width, 128, wnorm=ff_weight_norm),
             WNLinear(128, output_dim, wnorm=ff_weight_norm))
 
     def forward(self, x):
-        #grid = self.get_grid(x.shape, x.device)
-        #x = torch.cat((x, grid), dim=-1)  # [B, X, Y, Z, 4]
-        
         x = rearrange(x, 'b i s1 s2 s3 -> b s1 s2 s3 i')
-        x = self.in_proj(x)  # [B, X, Y, Z, H]
-        x = x.permute(0, 4, 1, 2, 3)  # [B, H, X, Y, Z]
-        x = F.pad(x, [0, self.padding, 0, self.padding, 0, self.padding])
-        x = x.permute(0, 2, 3, 4, 1)  # [B, X, Y, Z, H]
+        x = self.in_lift(x) # [B, X, Y, Z, H]
 
         for i in range(self.n_layers):
-            layer = self.spectral_layers[i]
-            b, _ = layer(x)
-            x = x + b
+            x = self.spectral_layers[i](x)
 
-        b = b[..., :-self.padding, :-self.padding, :-self.padding, :]
-        output = self.out(b)
-        output = rearrange(output, 'b s1 s2 s3 i -> b i s1 s2 s3')
+        x = self.out_proj(x)
+        x = rearrange(x, 'b s1 s2 s3 i -> b i s1 s2 s3')
 
-        return output
-
-    def get_grid(self, shape, device):
-        batchsize, size_x, size_y, size_z = shape[0], shape[1], shape[2], shape[3]
-        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
-        gridx = gridx.reshape(1, size_x, 1, 1, 1).repeat(
-            [batchsize, 1, size_y, size_z, 1])
-        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
-        gridy = gridy.reshape(1, 1, size_y, 1, 1).repeat(
-            [batchsize, size_x, 1, size_z, 1])
-        gridz = torch.tensor(np.linspace(0, 1, size_z), dtype=torch.float)
-        gridz = gridz.reshape(1, 1, 1, size_z, 1).repeat(
-            [batchsize, size_x, size_y, 1, 1])
-        return torch.cat((gridx, gridy, gridz), dim=-1).to(device)
+        return x
