89c654df58aebc0924bc0e66da13f3d827f5db3b
Marius Bohn <marius.bohn@t-online.de>
2025-07-07 12:22:18 +0200

    Fixed ObjectMaskPadding filter not on cuda device

--- magnet-pinn/magnet_pinn/losses/utils.py
+++ magnet-pinn/magnet_pinn/losses/utils.py
@@ -21,7 +21,7 @@ class MaskedLossReducer(torch.nn.Module):
 class ObjectMaskPadding:
     def __init__(self, padding: int = 1):
         self.padding = padding
-        self.padding_filter = torch.ones([1,1] + [self.padding*2 + 1]*3, dtype=torch.float32)
+        self.padding_filter = torch.ones([1,1] + [self.padding*2 + 1]*3, dtype=torch.float32).cuda()
 
     def __call__(self, input_shape_mask: torch.Tensor) -> torch.Tensor:
         check_border = torch.nn.functional.conv3d(input_shape_mask.type(torch.float32), self.padding_filter, padding=self.padding)
